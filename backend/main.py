3998c60760554f349c89adc35b1b283d16a00fe7
Sarthak Choubey <sarthakchoubey667@gmail.com>
2026-08-30 21:24:57 +0530

    Allow deployed frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -1966,8 +1966,9 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://disaster-command-center-1.onrender.com",
     ],
 
     allow_credentials=True,
